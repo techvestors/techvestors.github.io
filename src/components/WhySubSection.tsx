fa4f1dced85e565bdd3ab6b6370d680f9dfe8c78
Rakshank <rakshank.meena@gmail.com>
2023-04-10 02:46:18 +0530

    why section tabs gap fix

--- src/components/WhySubSection.tsx
+++ src/components/WhySubSection.tsx
@@ -12,7 +12,7 @@ const WhySubSection = () => {
     ]
     return (
         <div className='w-full md:px-[121px] h-auto flex justify-center flex-col md:gap-8 gap-5 mb-28 md:mb-64 items-center '>
-            <div className='flex md:gap-5 flex-col lg:flex-row'>
+            <div className='flex md:gap-8 gap-5 flex-col lg:flex-row'>
                 {wsArray.map((item: any, i: number) => {
                     if (i < 4) {
                         return (
@@ -21,7 +21,7 @@ const WhySubSection = () => {
                     }
                 })}
             </div>
-            <div className='flex md:gap-5 flex-col lg:flex-row'>
+            <div className='flex md:gap-8 gap-5 flex-col lg:flex-row'>
                 {wsArray.map((item: any, i: number) => {
                     if (i >= 4) {
                         return (
